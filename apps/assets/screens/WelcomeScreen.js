7260f4cd62b3bb046049ec4def5387835fef31fc
Zappingmadnnes <roberts.mezals@gmail.com>
2021-08-19 02:48:39 +0300

    Welcome Screen sākšana

--- apps/assets/screens/WelcomeScreen.js
+++ apps/assets/screens/WelcomeScreen.js
@@ -1,6 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, Image, TextInput, Button } from 'react-native';
 
-export default function App() {
+function WelcomeScreen(props) {
+
+    return (
+    <SafeAreaView style={styles.container}>
+        <Image style={styles.image} source={require("../Prakt_Logo.png")}/>
+        <Text style={styles.title}>PRAKT</Text>
+        <TextInput style={[styles.form, {top: 220}]} placeholder="Email"/>
+        <TextInput style={[styles.form, {top: 240}]} placeholder="Password"/>
+    </SafeAreaView>
+    );
 }
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        backgroundColor: 'dodgerblue',
+        flex: 1,
+    },
+    image: {
+        width: 150,
+        height: 150,
+        top: 75,
+        position: 'absolute',
+    },
+    title: {
+        fontSize: 30,
+        top: 190,
+    },
+    form: {
+        width: 260,
+        height: 40,
+        alignContent: 'center',
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        paddingLeft: 10,
+        fontSize: 20,
+    },
+    button: {
+        backgroundColor: 'red',
+    }
+})
+export default WelcomeScreen;
