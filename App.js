0ebe9934a4c65ca6f06d3157daf4633f333a3def
Zappingmadnnes <roberts.mezals@gmail.com>
2021-08-19 17:54:41 +0300

    Welcome Screen Improvements
    Redid layout of welcome screen using flexbox so it should look normal on diffrent sized screens.

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MainScreen from './apps/assets/screens/MainScreen';
 import WelcomeScreen from './apps/assets/screens/WelcomeScreen';
 
 export default function App() {
--- apps/assets/screens/WelcomeScreen.js
+++ apps/assets/screens/WelcomeScreen.js
@@ -1,45 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, Image, TextInput, Button } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, Image, TextInput, Pressable, View, YellowBox,} from 'react-native';
 
 function WelcomeScreen(props) {
 
     return (
     <SafeAreaView style={styles.container}>
+        <View style={styles.logoView}>
         <Image style={styles.image} source={require("../Prakt_Logo.png")}/>
-        <Text style={styles.title}>PRAKT</Text>
-        <TextInput style={[styles.form, {top: 220}]} placeholder="Email"/>
-        <TextInput style={[styles.form, {top: 240}]} placeholder="Password"/>
+        <Text style={{fontSize: 30}}> PRAKT </Text>
+        </View>
+
+        <View style={styles.loginView}>
+         <View style={styles.InputView}>
+         <TextInput style={styles.textInput} placeholder='Email'/>
+         <TextInput style={styles.textInput} placeholder='Password'/>
+         <Pressable style={styles.pressable} onPress={()=> console.log('Log In!')}>
+             <Text style={[{fontSize: 25},{color: '#fff'}]}> Log in </Text>
+         </Pressable>
+         </View>
+        </View>
     </SafeAreaView>
     );
 }
 const styles = StyleSheet.create({
     container: {
-        alignItems: 'center',
         backgroundColor: 'dodgerblue',
         flex: 1,
+    },
+    logoView: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+        
+    },
+    loginView: {
+        flex: 2,
+    },
+    InputView: {
+        flex: 0.4,
+        justifyContent: 'space-evenly',
+        paddingHorizontal: 40,
+    },
+    textInput: {
+        fontSize: 20,
+        backgroundColor: '#fff',
+        borderRadius: 20,
+        paddingHorizontal: 20,
+        height: 40,
     },
     image: {
         width: 150,
         height: 150,
-        top: 75,
-        position: 'absolute',
     },
-    title: {
-        fontSize: 30,
-        top: 190,
-    },
-    form: {
-        width: 260,
+    pressable: {
+        backgroundColor: '#0076a3',
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
         height: 40,
-        alignContent: 'center',
-        backgroundColor: '#fff',
-        borderRadius: 10,
-        paddingLeft: 10,
-        fontSize: 20,
-    },
-    button: {
-        backgroundColor: 'red',
     }
+    
 })
 export default WelcomeScreen;
