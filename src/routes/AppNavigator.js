129b3b52fb9bd974670039a4a75e92e39d18f6db
Zappingmadnnes <roberts.mezals@gmail.com>
2021-09-22 00:03:16 +0300

    Pamazam
    Pamazam partaisu marko cursed code uz react navigation

--- src/routes/AppNavigator.js
+++ src/routes/AppNavigator.js
@@ -2,16 +2,25 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import HomeScreen from "../screens/HomeScreen";
 import ProfileScreen from "../screens/ProfileScreen";
+import MatchesScreen from "../screens/MatchesScreen";
+
+import {StyleSheet } from "react-native";
+
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const HomeNavigator = () => (
   <Tab.Navigator>
-    <Tab.Screen name="HomeScreen" component={HomeScreen} />
-    <Tab.Screen name="Profile" component={ProfileScreen} />
+    <Tab.Screen name="1" component={HomeScreen} />
+    <Tab.Screen name="2" component={MatchesScreen} />
+    <Tab.Screen name="3" component={ProfileScreen} options={{tabBarIcon: () => (
+     <FontAwesome name="user" size = {30} style={styles.tabbarstyle}/>),}}/>
+
   </Tab.Navigator>
 );
 
@@ -20,3 +29,9 @@ export const AppNavigator = () => (
     <HomeNavigator />
   </NavigationContainer>
 );
+
+const styles = StyleSheet.create({
+  tabbarstyle: {
+    top: 10
+  }
+})
