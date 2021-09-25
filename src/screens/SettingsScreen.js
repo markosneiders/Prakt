d35c76169d2d5d69eef8d8bc694e615caa05ef83
Zappingmadnnes <roberts.mezals@gmail.com>
2021-09-25 23:18:01 +0300

    Revert "Revert "Revert "Update SettingsScreen.js"""
    This reverts commit d0ff3af0fa29ac276b8d90f08a474bf965e65ce8.

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,29 +1,23 @@
 import React from "react";
-import { ScrollView, StyleSheet, Pressable, Text } from "react-native";
+import {ScrollView, StyleSheet} from "react-native";
 import ProfileSettingsCard from "../components/ProfileSettingsCard/Index";
-import { Auth } from "aws-amplify";
 
-function SettingsScreen({ navigation }) {
-  return (
-    <ScrollView style={styles.container}>
-      <ProfileSettingsCard navigation={navigation} />
-      <Pressable onPress={() => Auth.signOut()} style={styles.button}>
-        <Text>Sign out</Text>
-      </Pressable>
-    </ScrollView>
-  );
+
+
+function SettingsScreen({navigation}) {
+    return(
+        <ScrollView style={styles.container}>
+            <ProfileSettingsCard navigation={navigation}/>
+        </ScrollView>
+        
+    );
 }
 
+
 const styles = StyleSheet.create({
-  container: {},
-  button: {
-    backgroundColor: "#34a9c9",
-    height: 25,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 20,
-    margin: 10,
-  },
-});
+    container: {
+    }
+})
+
 
 export default SettingsScreen;
