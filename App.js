45dbf942ea6a1c0ea515ab1bfd4848ad1b698e44
Zappingmadnnes <roberts.mezals@gmail.com>
2021-09-25 23:10:51 +0300

    Revert "Added back-end config"
    This reverts commit eaf0b4f9da39d5af5f597f103f6c1b30e42764e2.

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { AppNavigator } from "./src/routes/AppNavigator";
-import Amplify from "aws-amplify";
-import config from "./src/aws-exports";
-import { withAuthenticator } from "aws-amplify-react-native";
 
-Amplify.configure(config);
-
-function App() {
-  return <AppNavigator />;
+function App(){
+  return (
+          <AppNavigator />
+  );
 }
 
-export default withAuthenticator(App);
+
+export default App;
