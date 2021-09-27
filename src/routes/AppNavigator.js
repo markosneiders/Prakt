8a6c0eb816a8bfa74e73f8fc2fbdc58d2bf2339c
Zappingmadnnes <roberts.mezals@gmail.com>
2021-09-27 22:36:20 +0300

    Home Screen improvments

--- src/routes/AppNavigator.js
+++ src/routes/AppNavigator.js
@@ -4,7 +4,6 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { AnimatedTabBarNavigator } from "react-native-animated-nav-tab-bar";
 import { StyleSheet } from "react-native";
 
 //importing screens for navigator to use
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,10 +3,8 @@ import React, { useState } from "react";
 
 import data from "../assets/data/data";
 import Swiper from "react-native-deck-swiper";
-import { View, StyleSheet, Image, Text, useWindowDimensions} from "react-native";
-import { ImageResizeMode } from "react-native";
-
-
+import { View, StyleSheet, Image, Text, useWindowDimensions, TouchableOpacity} from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
 const Card =({card}) => (
     <View style={styles.card}>
         <Image source={{uri: card.image}} style={styles.cardImage}/>
@@ -23,17 +21,26 @@ export default function MainScreen() {
             <View style={styles.backgroundContainer}>
             <Image source={require("../assets/images/Maxima.jpg")}  blurRadius={3} style={styles.backgroundImage} resizeMode="cover"/>
             </View>
-            <Swiper 
+            <Swiper
+            ref={swiper => {
+                swiper = swiper
+              }}
             backgroundColor="transparent"
             cards={data}
             cardIndex={index}
             renderCard={(card) => <Card card = {card}/>}
-            stackSize={5}
-            stackScale={20}
-            stackSeparation={20}
+            stackSize={4}
+            stackScale={10}
+            stackSeparation={35}
             disableTopSwipe
             disableBottomSwipe
             />
+            <View  style={styles.buttoncontainer}>
+            <TouchableOpacity><Ionicons name="ios-close-circle-outline" size={80} color={'rgba(255,0,0,0.7)'}/></TouchableOpacity>
+            <TouchableOpacity><Ionicons name="md-arrow-back-circle-outline" size={80} color={'rgba(255,255,255,0.7)'} /></TouchableOpacity> 
+            <TouchableOpacity><Ionicons name="ios-checkmark-circle-outline" size={80} color={'rgba(0,255,0,0.7)'}/></TouchableOpacity>
+            </View>
+  
         </View>
     );
 }
@@ -43,11 +50,11 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,  
         alignItems: "center",
-        justifyContent: "center",
+        justifyContent: "flex-end",
         backgroundColor: "#FFF",
     },
     card: {
-        flex: 0.5,
+        flex: 0.7,
         borderRadius: 8,
         shadowRadius: 25,
         shadowColor: "#000",
@@ -69,11 +76,14 @@ const styles = StyleSheet.create({
         left: 0,
         right: 0,
         justifyContent: "center",
-        backgroundColor: "green"
       },
     backgroundImage: {
         alignSelf:"center",
         width: 1000,
         flex: 1,
+    },
+    buttoncontainer: {
+        flexDirection: "row",
+        marginBottom: 40,
     }
 })
