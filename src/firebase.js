ecb7ce5732c9a2fa808c34206cc50dc5ae4a9b28
markosneiders <67235193+markosneiders@users.noreply.github.com>
2021-09-28 23:23:44 +0300

    Firebase - Initialized

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+
+// Optionally import the services that you want to use
+//import "firebase/auth";
+//import "firebase/database";
+//import "firebase/firestore";
+//import "firebase/functions";
+//import "firebase/storage";
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyDocpv9l9rBI9qcAH8zeIBj2b-FEuMG-sI",
+  authDomain: "praktify.firebaseapp.com",
+  projectId: "praktify",
+  storageBucket: "praktify.appspot.com",
+  messagingSenderId: "823938196336",
+  appId: "1:823938196336:web:832f0d1f930f0cad2e25b5",
+  measurementId: "G-M246NSMR9X",
+});
