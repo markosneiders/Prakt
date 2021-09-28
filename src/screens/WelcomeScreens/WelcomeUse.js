58a4acd27ed8107aa3c19ab2ba65fabdcacaecb1
BigDDendijs <90863966+BigDDendijs@users.noreply.github.com>
2021-09-29 00:40:10 +0300

    adad

--- src/screens/WelcomeScreens/WelcomeUse.js
+++ src/screens/WelcomeScreens/WelcomeUse.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { View, Text } from "react-native";
 
-function WelcomeUse(props) {}
+function WelcomeUse(props) {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+}
 
 export default WelcomeUse;
