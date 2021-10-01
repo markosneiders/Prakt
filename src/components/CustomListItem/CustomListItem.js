f16159bfa539071bd075ec985cf7bf0fbbd0034c
markosneiders <67235193+markosneiders@users.noreply.github.com>
2021-10-01 14:31:20 +0300

    Chat functionality finished

--- src/components/CustomListItem/CustomListItem.js
+++ src/components/CustomListItem/CustomListItem.js
@@ -1,22 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { ListItem, Avatar } from "react-native-elements";
+import { db } from "../../firebase";
 
 const CustomListItem = ({ id, chatName, enterChat }) => {
+  const [chatMessages, setChatMessages] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = db
+      .collection("chats")
+      .doc(id)
+      .collection("messages")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setChatMessages(snapshot.docs.map((doc) => doc.data()))
+      );
+    return unsubscribe;
+  }, []);
+
   return (
-    <ListItem onPress={() => enterChat(id, chatName)} key={id} bottomDivider>
+    <ListItem
+      key={id}
+      onPress={() => enterChat(id, chatName)}
+      key={id}
+      bottomDivider
+    >
       <Avatar
         rounded
         source={{
-          uri: "http://d279m997dpfwgl.cloudfront.net/wp/2020/02/krivak-1-1000x750.jpg",
+          uri:
+            chatMessages?.[0]?.photoURL ||
+            "https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg",
         }}
       />
       <ListItem.Content>
         <ListItem.Title style={{ fontWeight: "800" }}>
-          {chatName}
+          {chatMessages?.[0]?.displayName}
         </ListItem.Title>
         <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail">
-          hello
+          {chatMessages?.[0]?.message}
         </ListItem.Subtitle>
       </ListItem.Content>
     </ListItem>
--- src/screens/InChatScreen.js
+++ src/screens/InChatScreen.js
@@ -22,15 +22,19 @@ import { db, auth } from "../firebase";
 
 const InChatScreen = ({ navigation, route }) => {
   const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      headerTitleAlign: "left",
       title: "Chat",
+      headerTintColor: "white",
+      headerStyle: {
+        backgroundColor: "#2755c2",
+      },
       headerBackTitleVisible: false,
 
       headerTitle: () => (
-        <View style={styles.header__info}>
+        <View>
           <View
             style={{
               flexDirection: "row",
@@ -40,17 +44,26 @@ const InChatScreen = ({ navigation, route }) => {
             <Avatar
               rounded
               source={{
-                uri: "http://d279m997dpfwgl.cloudfront.net/wp/2020/02/krivak-1-1000x750.jpg",
+                uri:
+                  messages[0]?.data.photoURL ||
+                  "https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg",
               }}
             />
-            <Text style={{ color: "black", marginLeft: 10, fontWeight: "700" }}>
-              {route.params.chatName}
+            <Text
+              style={{
+                color: "white",
+                marginLeft: 10,
+                fontWeight: "700",
+                marginRight: 230,
+              }}
+            >
+              Chat
             </Text>
           </View>
         </View>
       ),
     });
-  }, [navigation]);
+  }, [navigation, messages]);
 
   const sendMessage = () => {
     Keyboard.dismiss();
@@ -66,8 +79,25 @@ const InChatScreen = ({ navigation, route }) => {
     setInput("");
   };
 
+  useLayoutEffect(() => {
+    const unsubscribe = db
+      .collection("chats")
+      .doc(route.params.id)
+      .collection("messages")
+      .orderBy("timestamp", "asc")
+      .onSnapshot((snapshot) =>
+        setMessages(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+    return unsubscribe;
+  }, [route]);
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
       <StatusBar style="auto" />
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
@@ -75,7 +105,50 @@ const InChatScreen = ({ navigation, route }) => {
         keyboardVerticalOffset={90}
       >
         <>
-          <ScrollView></ScrollView>
+          <ScrollView contentContainerStyle={{ paddingTop: 15 }}>
+            {messages.map(({ id, data }) =>
+              data.email === auth.currentUser.email ? (
+                <View key={id} style={styles.receiver}>
+                  <Avatar
+                    position="absolute"
+                    rounded
+                    containerStyle={{
+                      position: "absolute",
+                      bottom: -15,
+                      right: -5,
+                    }}
+                    bottom={-15}
+                    right={-5}
+                    size={30}
+                    source={{
+                      uri: data.photoURL,
+                    }}
+                  />
+                  <Text style={styles.receiverText}>{data.message}</Text>
+                </View>
+              ) : (
+                <View key={id} style={styles.sender}>
+                  <Avatar
+                    position="absolute"
+                    rounded
+                    containerStyle={{
+                      position: "absolute",
+                      bottom: -15,
+                      right: -5,
+                    }}
+                    bottom={-15}
+                    right={-5}
+                    size={30}
+                    source={{
+                      uri: data.photoURL,
+                    }}
+                  />
+                  <Text style={styles.senderText}>{data.message}</Text>
+                  <Text style={styles.senderName}>{data.displayName}</Text>
+                </View>
+              )
+            )}
+          </ScrollView>
           <View style={styles.footer}>
             <TextInput
               value={input}
@@ -98,6 +171,43 @@ export default InChatScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  receiver: {
+    padding: 15,
+    backgroundColor: "#2B68E6",
+    alignSelf: "flex-end",
+    borderRadius: 20,
+    marginRight: 15,
+    marginBottom: 20,
+    maxWidth: "80%",
+    position: "relative",
+  },
+  sender: {
+    padding: 15,
+    backgroundColor: "#ECECEC",
+    alignSelf: "flex-start",
+    borderRadius: 20,
+    marginRight: 15,
+    marginBottom: 20,
+    maxWidth: "80%",
+    position: "relative",
+  },
+  senderText: {
+    color: "black",
+    fontWeight: "500",
+    marginLeft: 10,
+    marginBottom: 8,
+  },
+  receiverText: {
+    color: "white",
+    fontWeight: "500",
+    marginLeft: 10,
+  },
+  senderName: {
+    left: 10,
+    paddingRight: 10,
+    fontSize: 10,
+    color: "black",
   },
   footer: {
     flexDirection: "row",
